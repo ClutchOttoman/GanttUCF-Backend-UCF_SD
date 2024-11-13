76191f6595f046b3a531c5e50e207f012548c961
ShadowBoxing7 <ashkoltz91@gmail.com>
2024-11-13 14:01:51 -0500

    Update make-data-key.js

--- make-data-key.js
+++ make-data-key.js
@@ -20,6 +20,32 @@ const kms_providers = {
 
 // Generate key.
 async function main(){
-  const uri = "process.env.MONGO_DB_URI";
+  const uri_string = "process.env.MONGO_DB_URI";
+
+  // Set up the database and collection containing key.
+  const master_key_database_name = "keys_database";
+  const master_key_collection_name = "key_collection";
+  const master_key_namespace = '${master_key_database_name}.${master_key_collection_name}';
+  const master_key_client = new MongoClient(uri_string);
+  await master_key_client.connect();
+  const master_key_client_database = master_key_client.collection(master_key_collection_name);
+
+  await master_key_client_database.createIndex(
+    {keyAltNames: 1},
+    {unique: true, partialExpressionFilter: {keyAltNames: {$exists: true}}},
+  );
+
+  // Generate key.
+  const master_key_generate = new MongoClient(uri_string);
+  await master_key_generate.connect();
+  const encrypt = new ClientEncryption(master_key_generate, {master_key_namespace, kms_providers});
+  const generated_key = await encrypt.createDataKey("local");
+  console.log();
+
+  // Close services when done.
+  await master_key_client.close();
+  await master_key_generate.close();
   
 }
+
+main();
