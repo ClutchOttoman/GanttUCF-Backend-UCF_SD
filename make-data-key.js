476b30f41cf5b2ce1d6a4591f97b1ef76b73f711
ShadowBoxing7 <ashkoltz91@gmail.com>
2024-11-13 13:14:54 -0500

    Updated make-data-key.js

--- make-data-key.js
+++ make-data-key.js
@@ -1,1 +1,25 @@
+// Note: This and the master key should not be stored locally in the Github. For better security, please use a Key Management Service (KMS) such as Azure.
 
+const {MongoClient, Binary, ClientEncryption} = require("mongodb");
+const file = require("fs");
+const crypto = require("crypto");
+
+// Create text file to store master key.
+try {
+  file.writeFileSync("csfle-master-key.txt", crypto.randomBytes(96));
+catch (error){
+  console.error(error);
+}
+
+const provider = "local";
+const path = "./csfle-master-key.txt";
+const master_local_key = file.readFileSync(path);
+const kms_providers = {
+  local {key: master_local_key,},
+};
+
+// Generate key.
+async function main(){
+  const uri = "process.env.MONGO_DB_URI";
+  
+}
