c1362b9b57baa86a0fb720212fb2d4282137552a
Shane Welz <shanewelz@gmail.com>
2024-11-21 18:07:21 +0000

    Maybe adding a binary in server.js might help?

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require("express"Object.);
 const { MongoClient, ObjectId, Timestamp, Binary } = require("mongodb");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
