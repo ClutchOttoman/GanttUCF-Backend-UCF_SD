d2e47f170c9af1621bb18bf948cc8163705608ec
Shane Welz <shanewelz@gmail.com>
2024-12-05 17:45:06 +0000

    Fix to server.js?

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 require('dotenv').config();
-const { MongoClient, Binary } = require('mongodb');
+const {MongoClient} = require('mongodb');
 
 const PORT = process.env.PORT || 5000;
 const url = process.env.MONGODB_URI;
